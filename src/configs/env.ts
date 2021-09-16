62d17a6e156a6ad560356675c8dff5a2f21a11ae
ryanlemes <ryanllemes@gmail.com>
2021-09-15 21:22:04 -0300

    Default env configuration

---
+++ src/configs/env.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'development' ? '.env.test' : '.env',
+});
+
+export default {
+  port: process.env.PORT || 3000,
+  service: {
+    name: process.env.SERVICE_NAME,
+    code_version: process.env.CODE_VERSION || '1.0.0',
+  },
+  pactEnv: {
+    broker: {
+      url: process.env.PACT_BROKER_URL,
+      username: process.env.PACT_BROKER_USERNAME,
+      password: process.env.PACT_BROKER_PASSWORD,
+      token: process.env.PACT_BROKER_TOKEN,
+    },
+  },
+};
