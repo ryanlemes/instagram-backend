834a7c5c4e0b6e0476e9715b21d91d4ba710f26b
ryanlemes <ryanllemes@gmail.com>
2021-09-15 23:51:19 -0300

    Create post routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,18 @@
-import Router from 'koa-router';
+import Router from '@koa/router';
+import multer from '@koa/multer';
+
 import StatusController from '../app/controllers/statusController';
+import PostController from '../app/controllers/postController';
 
 const router = new Router();
+const upload = multer();
 
 const status = StatusController();
+const post = PostController();
 
 router.get('/health', status.health);
+router.get('/posts', post.index);
+router.post('/posts', upload.single('image'), post.store);
+router.delete('/posts/:id', post.remove);
 
 export default router;
