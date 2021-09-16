638b3562e8ad5d14d8497a2fcf492b6609608526
ryanlemes <ryanllemes@gmail.com>
2021-09-15 23:48:15 -0300

    Generate post service interface

---
+++ src/app/services/post/interfaces/postServiceInterface.ts
@@ -0,0 +1,19 @@
+export interface ServiceResponse {
+  status: number,
+  message?: string,
+  data?: any,
+}
+
+export interface PostBody {
+  author: string,
+  place: string,
+  description: string,
+  hashtags: string,
+  image: string,
+}
+
+export interface PostService {
+  findAll: () => Promise<ServiceResponse>;
+  createNew: () => Promise<ServiceResponse>;
+  deleteOne: () => Promise<ServiceResponse>;
+}
