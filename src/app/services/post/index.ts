b7f83815fbba90c63ee010876739651de886ce10
ryanlemes <ryanllemes@gmail.com>
2021-09-28 19:24:00 -0300

    Add update post update method

--- src/app/services/post/index.ts
+++ src/app/services/post/index.ts
@@ -38,13 +38,39 @@ function PostService() {
   };
 
   /**
+   * Update a post
+   * @param id string that contains mongoose id
+   * @param body Post body object that contains all
+   * parameters to update the post
+   * @returns the post object updated
+   */
+  const updateOne = async (id: string, body: PostBody): Promise<ServiceResponse> => {
+    const post = await Post.findOne({ id });
+
+    const update: any = {};
+    post.author != body.author ? update.author = body.author : null;
+    post.place != body.place ? update.place = body.place : null;
+    post.description != body.description ? update.description = body.description : null;
+    post.hashtags != body.hashtags ? update.hashtags = body.hashtags : null;
+    post.image != body.image ? update.image = body.image : null;
+    post.likes != body.likes ? update.likes = body.likes : null;
+
+    await post?.updateOne(update);
+
+    return {
+      status: 200,
+      message: undefined,
+      data: post,
+    };
+  };
+
+  /**
    * Remove some post based on id
    * @param id string that contains mongoose id
    * @returns a message if the post was removed
    */
   const deleteOne = async (id: string): Promise<ServiceResponse> => {
-    const post = await findOne(id);
-    post?.delete();
+    await Post.findByIdAndRemove(id);
 
     return {
       status: 200,
@@ -57,6 +83,7 @@ function PostService() {
     findOne,
     findAll,
     createNew,
+    updateOne,
     deleteOne,
   };
 }
