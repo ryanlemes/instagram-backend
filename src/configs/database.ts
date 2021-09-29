e216dfb59ee5d83a602be27730faa502b84cc87f
ryanlemes <ryanllemes@gmail.com>
2021-09-28 21:11:47 -0300

    fixup database config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 import env from './env';
 
 // Mongoose connection
-mongoose.connect(env.database.url || '', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, (err) => {
+mongoose.connect(env.database.url || '', {}, (err) => {
   if (err) {
     console.log(err);
   }
